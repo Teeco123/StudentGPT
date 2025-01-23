33920474899503f23efe67073d57fc0a57fe4999
Kacper Gajko <kacper.gajko1@icloud.com>
2025-01-23 01:20:28 +0100

    Initial Commit

---
+++ StudentGPT/StudentGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentGPTApp.swift
+//  StudentGPT
+//
+//  Created by Kacper on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct StudentGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
