55854c21e49a957e9e57bc104147dd75ab07a7f8
Kacper Gajko <kacper.gajko1@icloud.com>
2025-01-24 22:00:10 +0100

    fixed paddings for text box

--- StudentGPT/Views/ChatView/ChatView.swift
+++ StudentGPT/Views/ChatView/ChatView.swift
@@ -40,26 +40,26 @@ struct ChatView: View {
             HStack(){
                 Image(systemName: "plus.circle")
                     .foregroundColor(Color.white)
-                    .padding([.top, .leading, .bottom], 10)
-                    .padding(.trailing, 5)
+                    .padding()
+                    .padding(.trailing, -5.0)
                     .font(.system(size: 20))
                 TextField("Message...", text: $inputText)
                     .foregroundColor(.white)
-                    .padding(.trailing, 50)
+                    .padding()
+                    .padding(.horizontal, -20.0)
                 Button(action: {
                     SendMessage()
                 }){
                     Image(systemName: "paperplane")
                         .foregroundColor(Color.white)
-                        .padding(.vertical, 10)
-                        .padding(.trailing, 20.0)
-                        .padding(.leading, -40.0)
+                        .padding()
                         .font(.system(size: 20))
                 }
             }
             .frame(maxWidth: 370, maxHeight: 45, alignment: .leading)
             .background(Color(red: 0.1, green: 0.1, blue: 0.1))
             .cornerRadius(18)
+            .padding()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.black)
